ac55523db7082db7730048ff88e6c05233cd4e58
alberto cruz mtz <albertocruz8133@proton.me>
2026-05-20 22:10:40 -0600

    agrega configuracion para waybar con diseño minimalista inspirado en omarchy

--- config/waybar/scripts/keyboard-layout.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-hyprctl -j devices | jq -r '.keyboards[] | select(.main == true) | .active_keymap' | sed \
-  -e 's/English (US)/us/' \
-  -e 's/Spanish (Latin American)/es/' \
-  -e 's/Spanish (Latam)/latam/'
--- config/waybar/scripts/toggle-layout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Obtiene el nombre del teclado principal
-device=$(hyprctl -j devices | jq -r '.keyboards[] | select(.main == true) | .name')
-
-# Cambia el layout del teclado principal
-hyprctl switchxkblayout "$device" next
