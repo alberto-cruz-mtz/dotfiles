5267cb93f97caea72efef9290c4c67fd2f5b62b6
jose alberto cruz mtz <albertocruz8133@proton.me>
2026-08-28 13:59:22 -0600

    corrige error de alias para ls usando el paqueta 'eza'
    El error sucedia porque 'eza' agrego un nuevo argumento o valor que no
    estaba en la version anterior por lo tanto para solucionar el error de
    ls al listar el contenido de directorios internos se agrego 'always' al
    alias

--- config/fish/functions/ls.fish
+++ config/fish/functions/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps='eza --long --header --level 1 --no-permissions --no-user --tree --group-directories-first --git --color' --wraps='eza --long --header --level 1 --no-permissions --no-user --tree --group-directories-first --git --color --icons' --description 'alias ls=eza --long --header --level 1 --no-permissions --no-user --tree --group-directories-first --git --color --icons'
-    eza --long --header --level 1 --no-permissions --no-user --tree --group-directories-first --git --color --icons $argv
+function ls --wraps='eza --long --header --level 1 --no-permissions --no-user --tree --group-directories-first --git --color' --wraps='eza --long --header --level 1 --no-permissions --no-user --tree --group-directories-first --git --color --icons' --wraps='eza --long --header --level 1 --no-permissions --no-user --tree --group-directories-first --git --color --icons always' --description 'alias ls=eza --long --header --level 1 --no-permissions --no-user --tree --group-directories-first --git --color --icons always'
+    eza --long --header --level 1 --no-permissions --no-user --tree --group-directories-first --git --color --icons always $argv
 end
