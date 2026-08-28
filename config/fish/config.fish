c17b71fc6a565e8941e2d4eac97e1a3a1689b337
jose alberto cruz mtz <albertocruz8133@proton.me>
2026-08-28 14:01:35 -0600

    agrega nuevas variables y cambia la inicializacion de tmux a zellij
    - Agrega variable global para PNPM ya que causaba un mensaje de error al
    instalar paquetes de forma global
    - Agrega variable TERM ya que zellij con PI tenian un problema al ya que
    despues de cerrar sesion los colores desaparecian dado que no sabian con
    que sistema trabajar si ghostty o xterm-256color

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,22 +7,34 @@ if test -n "$TERMUX_VERSION"; or test -d /data/data/com.termux
     set IS_TERMUX 1
 end
 
-if not set -q TMUX
-    if type -q tmux
-        tmux new-session -A -s main
-    else if type -q zellij
+if not set -q TMUX; and not set -q ZELLIJ
+    # if type -q tmux
+    #     tmux new-session -A -s main
+    # else if type -q zellij
+    #     zellij attach -c main
+    # end
+
+    if type -q zellij
         zellij attach -c main
+    else if type -q tmux
+        tmux new-session -A -s main
     end
 end
-
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+# pnpm
+set --export PNPM_HOME "$HOME/.local/share/pnpm"
+set --export PATH $PNPM_HOME/bin $PATH
+# xterm
+set -gx TERM xterm-256color
 
-set -gx PROJECT_PATHS ~/workspace ~/.config
+# Plugin pj
+set -gx PROJECT_PATHS ~/workspace ~/.config ~/IdeaProjects/
 
 starship init fish | source
 
 fastfetch
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
